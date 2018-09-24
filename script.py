618f88a6404a9a00014397a9a111af18f0483ec3
FVT375 <jingurn34v@gmail.com>
2018-09-24 21:58:56 +0300

    + script

--- script.py
+++ script.py
@@ -9,21 +9,24 @@ def to_grayscale():
     imgmat = np.matrix(imgmat)
     U, sigma, V = np.linalg.svd(imgmat)
     reconstimg = np.matrix(U[:, :n+1]) * np.diag(sigma[:n+1]) * np.matrix(V[:n+1, :])
-    reconstimg.save(path + "_grayscale.jpg")
+    reconstimgGS = Image.fromarray(np.uint8(reconstimg))
+    reconstimgGS.save(image_name + "_grayscale.jpg")
+    print('Done. Compression rate is {0:.2f}%'.format(100 - compression))
+
     
 def to_rgb():
     imgrgb = img.convert('RGB')
 
-    imgR = np.empty((imgX, imgY))
-    imgG = np.empty((imgX, imgY))
-    imgB = np.empty((imgX, imgY))
+    imgR = np.empty((imgY, imgX))
+    imgG = np.empty((imgY, imgX))
+    imgB = np.empty((imgY, imgX))
 
     for i in range(imgX):
         for j in range(imgY):
             r, g, b = imgrgb.getpixel((i, j))
-            imgR[i][j] = r
-            imgG[i][j] = g
-            imgB[i][j] = b
+            imgR[j][i] = r
+            imgG[j][i] = g
+            imgB[j][i] = b
             
     imgR = np.matrix(imgR)
     imgG = np.matrix(imgG)
@@ -33,43 +36,35 @@ def to_rgb():
     UG, sigmaG, VG = np.linalg.svd(imgG)
     UB, sigmaB, VB = np.linalg.svd(imgB)
 
-    reconstimgR = np.matrix(UR[:, :2]) * np.diag(sigmaR[:2]) * np.matrix(VR[:2, :])
-    reconstimgG = np.matrix(UG[:, :2]) * np.diag(sigmaG[:2]) * np.matrix(VG[:2, :])
-    reconstimgB = np.matrix(UB[:, :2]) * np.diag(sigmaB[:2]) * np.matrix(VB[:2, :])
+    reconstimgR = np.matrix(UR[:, :n+1]) * np.diag(sigmaR[:n+1]) * np.matrix(VR[:n+1, :])
+    reconstimgG = np.matrix(UG[:, :n+1]) * np.diag(sigmaG[:n+1]) * np.matrix(VG[:n+1, :])
+    reconstimgB = np.matrix(UB[:, :n+1]) * np.diag(sigmaB[:n+1]) * np.matrix(VB[:n+1, :])
 
     imgR = Image.fromarray(np.uint8(reconstimgR))
     imgG = Image.fromarray(np.uint8(reconstimgG))
     imgB = Image.fromarray(np.uint8(reconstimgB))
-
-    imgR = imgR.transpose(Image.ROTATE_270)
-    imgR = imgR.transpose(Image.FLIP_LEFT_RIGHT)
-    imgG = imgG.transpose(Image.ROTATE_270)
-    imgG = imgG.transpose(Image.FLIP_LEFT_RIGHT)
-    imgB = imgB.transpose(Image.ROTATE_270)
-    imgB = imgB.transpose(Image.FLIP_LEFT_RIGHT)
-
     reconstimgRGB = Image.merge("RGB", (imgR, imgG, imgB))
+    reconstimgRGB.save(image_name + "_rgb.jpg")
+    print('Done. Compression rate is {0:.2f}%'.format(100 - compression))
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', type=str)
+    parser.add_argument('n', type=int)
+    parser.add_argument('--grayscale', action='store_true')
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('image', type=str)
-parser.add_argument('n', type=int)
-parser.add_argument('--grayscale', action='store_true')
-args = parser.parse_args()
+    path = args.image
+    image_name = path.split('.')[0]
+    img = Image.open(args.image)
+    imgX, imgY = img.size[0], img.size[1]
 
-path = args.image
-img = Image.open(args.image)
-imgX, imgY = img.size[0], img.size[1]
+    n = args.n
 
-n = args.n
+    compression = ((imgX * n) + n + (imgY * n)) / (imgX * imgY)
+    compression *= 100
 
-compression = ((imgX * n) + n + (imgY * n)) / (imgX * imgY)
-compression *= 100
-
-if args.grayscale:
-    to_grayscale()
-else:
-    to_rgb()
-
-
-###
+    if args.grayscale:
+        to_grayscale()
+    else:
+        to_rgb()
